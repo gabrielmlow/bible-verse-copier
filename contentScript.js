a31732885860b4c2fcedbe613b5e70f02c10f90c
gabrielmlow <57755942+gabrielmlow@users.noreply.github.com>
2021-08-26 13:52:18 +0800

    fixed bugs and updated verse limit

--- contentScript.js
+++ contentScript.js
@@ -1,34 +1,29 @@
 let verse = "";
+let bible = "";
 let verseCount = 0;
+const verseLimit = 4;
 
 // Fix For French
 window.setInterval(function() {
 
     const currentVerse = document.getElementById("pVerse").innerText;
+    const currentBible = document.getElementById("pTranslation").innerText;
     // const UpdatePanel1 = document.getElementById("UpdatePanel1");
 
     // const footerEl = document.getElementsByClassName("footer");
 
-    if (verse !== currentVerse) {
+    if (verse !== currentVerse || bible !== currentBible) {
 
-        let content = "no verse";
-
-        if (document.getElementById("pContent").innerText != null) {
-            content = document.getElementById("pContent").innerText;
-        }
+        const content = document.getElementById("pContent").innerText != null ? document.getElementById("pContent").innerText : "no content";
         
         verse = currentVerse;
+        bible = currentBible;
 
-        if (verseCount >= 4) {
+        if (verseCount >= verseLimit) {
             document.body.removeChild(document.body.children[1]);
         } else {
             verseCount += 1
         }
-        // key += 1;
-        // chrome.storage.sync.set({key: [verse, content]}, function() {
-        //     console.log(verse);
-        // });
-        // chrome.storage.sync.set({key: [verse, content]});
         
         const verseText = document.createTextNode(verse);
         const contentText = document.createTextNode(content);
